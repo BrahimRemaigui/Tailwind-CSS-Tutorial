2ad35a0861162d417f54d36040201410f1db7846
Brahim Remaigui <119951262+BrahimRemaigui@users.noreply.github.com>
2023-12-17 23:08:34 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+<>
+    <div className="bg-blue-500 text-white p-4">
+      This is a component with Tailwind CSS styles.
+    </div>
+    <div className="w-fit font-body text-blue-500 text-4xl font-bold capitalize pl-20 pt-20 border-red-400 border-2 border-solid rounded-3xl bg-pramary"  >
+      hello i am brahim 
+    </div>
+
+    </>
+  );
+}
+
+export default App;
